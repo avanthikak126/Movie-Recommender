87e106e1d8c1b31f877af1b21805b35b086e2b7c
avanthikak126 <avanthikak126@gmail.com>
2026-07-10 21:32:53 +0530

    changes

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 DATABASE_URL = (
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"postgresql+pg8000://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 
